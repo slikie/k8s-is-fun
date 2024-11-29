bb35207e49d2c8b37925e4552d919487cc1c925d
RT <13197246+slikie@users.noreply.github.com>
2024-11-29 03:01:20 +0100

    Exercise 1.05: Project v0.3

--- Project-v0.1/index.js
+++ Project-v0.1/index.js
@@ -5,6 +5,10 @@ const app = express()
 const PORT = process.env.PORT || 23224
 
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 
 app.listen(PORT, 'localhost', () => {
   console.log(`Server started in port ${PORT}`);
